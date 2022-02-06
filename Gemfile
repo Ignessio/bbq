1c08e1e1908fd4d735fb2548f84cb8acde89f644
Valentin Zaborov <xerrox@yandex.ru>
2022-02-06 15:38:39 +0300

    Add devise to user

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '~> 6.1.4', '>= 6.1.4.4'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'webpacker', '~> 5.0'
+gem 'devise'
 
 group :development, :test do
   gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
-  EMAIL_FORMAT = /\A\w+@\w+\.\w+\z/
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :events, dependent: :destroy
 
+  before_validation :set_name, on: :create
+
   validates :name, presence: true, length: { maximum: 35 }
-  validates :email,
-    presence: true,
-    uniqueness: true,
-    length: { maximum: 255 },
-    format: { with: EMAIL_FORMAT }
+
+  private
+
+  def set_name
+    self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+  end
 end
