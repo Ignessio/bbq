c76c5935f46882c7b0714a691662a8416bcdad40
Valentin <xerrox@yandex.ru>
2022-02-07 12:34:34 +0300

    Add flash bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   private
 
   def set_name
-    self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+    self.name = "User#{rand(777)}" if self.name.blank?
   end
 end
