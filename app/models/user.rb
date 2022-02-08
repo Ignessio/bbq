613a6263cc8eb3958c827e780123a8d09d0d0660
Valentin <xerrox@yandex.ru>
2022-02-08 09:15:20 +0300

    Change flash messages. Remove default name from model user. Add error messages to new user nad new event forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,5 @@ class User < ApplicationRecord
 
   has_many :events, dependent: :destroy
 
-  before_validation :set_name, on: :create
-
   validates :name, presence: true, length: { maximum: 35 }
-
-  private
-  def set_name
-    self.name = "User#{rand(999)}" if self.name.blank?
-  end
 end
