3d8c3658e2365363a50a2dff7c409aa55a331c34
Valentin <xerrox@yandex.ru>
2022-02-10 14:54:40 +0300

    Add flash messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,23 @@ module ApplicationHelper
   def user_avatar(user)
     asset_pack_path('media/images/user.png')
   end
+
+  def flash_messages(name, message)
+    case name
+    when "alert"
+      <<~FLASH
+      <div id="alert" class="col-md-6 mt-4 alert alert-danger alert-dismissible fade show" role="alert">
+      #{message}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      FLASH
+    when "notice"
+      <<~FLASH
+      <div id="alert" class="col-md-6 mt-4 alert alert-success alert-dismissible fade show" role="notice">
+      #{message}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      FLASH
+    end
+  end
 end
