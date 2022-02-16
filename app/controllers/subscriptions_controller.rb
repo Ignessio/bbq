7b191e8fe7d87f1613155ebbade1ef1d221525c2
Valentin <xerrox@yandex.ru>
2022-02-16 08:33:19 +0300

    Add subscriptions callback before validation. Hide event guest list if empty.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -11,28 +11,27 @@ class SubscriptionsController < ApplicationController
     else
       render 'events/show', alert: t('.error')
     end
-    end
-
-    def destroy
-      message = { notice: t('.deleted') }
-      if current_user_can_edit?(@subscription)
-        @subscription.destroy
-      else
-        message = { alert: t('.error') }
-      end
-
-      redirect_to @event, message
-    end
-
-  private
-  def set_subscription
-    @subscription = @event.subscriptions.find(params[:id])
   end
 
+  def destroy
+    message = { notice: t('.deleted') }
+    if current_user_can_edit?(@subscription)
+      @subscription.destroy
+    else
+      message = { alert: t('.error') }
+    end
+    redirect_to @event, message
+  end
+
+  private
   def set_event
     @event = Event.find(params[:event_id])
   end
   
+  def set_subscription
+    @subscription = @event.subscriptions.find(params[:id])
+  end
+
   def subscription_params
     params.fetch(:subscription, {}).permit(:user_email, :user_name)
   end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,9 +4,11 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :user_name, presence: true, unless: -> { user.present? }
+  before_validation :event_owner
 
   validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
+
+  validates :user_name, presence: true, unless: -> { user.present? }
 
   validates :user_email,
           presence: true,
@@ -14,19 +16,15 @@ class Subscription < ApplicationRecord
           format: { with: EMAIL_FORMAT },
           unless: -> { user.present? }
 
+  def event_owner
+    errors if event.user == user
+  end
+
   def user_name
-    if user.present?
-      user.name
-    else
-      super
-    end
+    user&.name || super
   end
 
   def user_email
-    if user.present?
-      user.email
-    else
-      super
-    end
+    user&.email || super
   end
 end
