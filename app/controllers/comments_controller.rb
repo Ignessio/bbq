6faac1b0c477b2f32ee4682095ef3821f50c81e5
Valentin <xerrox@yandex.ru>
2022-03-25 17:33:03 +0300

    Remove comment author from mail list

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -41,7 +41,7 @@ class CommentsController < ApplicationController
   end
 
   def notify_new_comment(comment)
-    all_emails = (comment.event.subscriptions.map(&:user_email) + [comment.event.user.email]).uniq
+    all_emails = (comment.event.subscriptions.map(&:user_email) + [comment.event.user.email] - [comment.user&.email]).uniq
 
     all_emails.each do |mail|
       EventMailer.comment(comment, mail).deliver_now
