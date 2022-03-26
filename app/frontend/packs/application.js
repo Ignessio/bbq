db163ec88cc56569f052e1bf365d4f4c6a0583b4
Valentin <xerrox@yandex.ru>
2022-03-26 12:22:40 +0300

    Corrected event model

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,6 +3,7 @@ Rails.start()
 
 import '../styles/application'
 import '../scripts/alert_hide'
-const images = require.context('../images', true)
+const images = require.context('../../frontend/images', true)
+const imagePath = name => images(name, true)
 
 import 'bootstrap/dist/js/bootstrap'
