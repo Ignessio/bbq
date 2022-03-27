f0b544ac81efada1bf5dac5741572c15e40c88c3
Valentin <xerrox@yandex.ru>
2022-03-27 11:30:20 +0300

    Delete carriewave, uploaders

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,8 +3,6 @@ class Photo < ApplicationRecord
   belongs_to :user
 
   validates :photo, presence: :true
-  
-  mount_uploader :photo, PhotoUploader
 
   scope :persisted, -> { where.not(id: nil) }
 end
