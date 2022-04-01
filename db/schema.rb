4721d7888f8fa679c8732a9c305fea7ffc15a0c6
Valentin <xerrox@yandex.ru>
2022-04-01 12:19:27 +0300

    Add pincode to event model, form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_114826) do
+ActiveRecord::Schema.define(version: 2022_04_01_090406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_03_19_114826) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
