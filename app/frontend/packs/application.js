22c7ec170793785bb7810c57810866407e8d0384
Valentin <xerrox@yandex.ru>
2022-04-01 18:18:45 +0300

    Add yandex map

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,6 +3,8 @@ Rails.start()
 
 import '../styles/application'
 import '../scripts/alert_hide'
+import '../scripts/map'
+
 const images = require.context('../images', true)
 
 import 'bootstrap/dist/js/bootstrap'
